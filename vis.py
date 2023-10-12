48bc856bfb362d1d0b00a2ad09c500ba57caebc8
Arif <arif@themascoteers.com>
2023-10-13 03:52:00 +1100

    done

--- vis.py
+++ vis.py
@@ -2,7 +2,7 @@ import pandas as pd
 from s2_bands import S2Bands
 import matplotlib.pyplot as plt
 
-band_columns = ["B01","B02","B03","B04","B05","B06","B07","B08","B8A","B09","B11"]#"B12"]
+band_columns = ["B02","B03","B04","B05","B06","B07","B08","B8A","B11","B12"]
 df = pd.read_csv(r"data\processed\S2A_MSIL2A_20220207T002711_N0400_R016_T54HWE_20220207T023040\csvs\ag.csv")
 rows = df.iloc[0:6]
 
