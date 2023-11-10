4874613113dcb5f91cf98e9090263a63dc06dc7f
arif <arif@themascoteers.com>
2023-11-11 09:20:38 +1100

    done

--- test.py
+++ test.py
@@ -2,7 +2,6 @@ from evaluator import Evaluator
 
 if __name__ == "__main__":
     c = Evaluator(prefix="shared", folds=10, algorithms=[
-        "ann_shared",
         "ann_shared_small"
     ])
     c.process()
