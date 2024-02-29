7e9a06d2e7fe6205b5d3d058e828f19c43f1e782
Arif <arif@themascoteers.com>
2024-02-29 17:16:05 +1100

    done

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
 from evaluator import Evaluator
 
 if __name__ == "__main__":
-    c = Evaluator(prefix="ann_simple", folds=10, algorithms=[
-        "ann_simple"
+    c = Evaluator(prefix="mixed", folds=3, algorithms=[
+        "ann_savi",
+        "ann_savi_skip",
+
+        "ann_savi_learnable",
+        "ann_savi_skip_learnable",
+        "ann_savi_skip_learnable_fn",
+        "ann_savi_skip_learnable_bi",
     ])
     c.process()
